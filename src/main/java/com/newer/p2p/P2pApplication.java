c7716c59fef6db14ff31f7728fd4d96472061d70
wangchao <wangchao4631@163.com>
2020-06-27 15:10:49 +0800

    /////////

--- src/main/java/com/newer/p2p/P2pApplication.java
+++ src/main/java/com/newer/p2p/P2pApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class P2pApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(P2pApplication.class, args);
     }
